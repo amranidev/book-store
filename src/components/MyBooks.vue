338930d451f4824fba14f13ab57dc52f4ef3c5bc
Amrani Houssain <amranidev@gmail.com>
2018-01-07 23:19:01 +0000

    update mybooks

--- src/components/MyBooks.vue
+++ src/components/MyBooks.vue
@@ -1,17 +1,36 @@
 <template>
     <div class="container">
-        <md-layout md-gutter md-align="center">
-            <md-whiteframe class="book-container">
-                <h1>{{Welcome}}</h1>
-            </md-whiteframe>
+        <md-layout md-gutter>
+            <md-layout v-for="(b,index) in books" :key="index" md-flex-small="100" md-flex-xsmall="100" md-flex-medium="30">
+                <md-card class="book-card">
+                    <md-card-header>
+                        <md-card-media md-medium>
+                            <img :src="b.cover" alt="People">
+                        </md-card-media>
+                        <div class="md-title">{{b.title}}</div>
+                        <div class="md-subhead">{{b.author}}</div>
+                    </md-card-header>
+
+                    <md-card-actions>
+                        <md-button>Action</md-button>
+                        <md-button>Action</md-button>
+                    </md-card-actions>
+
+                    <md-card-content>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                    </md-card-content>
+                </md-card>
+            </md-layout>
         </md-layout>
     </div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        console.log("component is ready!!")
+    firestore() {
+        return {
+            books: this.$store.state.firestore.collection("books").orderBy("created", "desc")
+        }
     },
     data() {
         return {
@@ -26,6 +45,11 @@ export default {
 
 <style scoped>
 .book-container {
-    width: 70%;
+    padding: 15px;
+    /*width: 70%;*/
+}
+
+.book-card {
+    margin: 10px;
 }
 </style>
